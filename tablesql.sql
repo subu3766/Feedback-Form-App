00eaed08b6b39b9c99f00c2dbdc0cf3cc7bd9f7c
subu3766 <subramanya3766@gmail.com>
2024-09-06 01:50:33 +0530

    Update tablesql.sql

--- tablesql.sql
+++ tablesql.sql
@@ -1,9 +1,10 @@
 CREATE TABLE feedback (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    student_id VARCHAR(255),
-    student_name VARCHAR(255),
+    student_id_hash VARCHAR(255) NOT NULL,
+    student_name_hash VARCHAR(255) NOT NULL,
     semester INT,
     subject VARCHAR(255),
     question VARCHAR(255),
-    rating INT
+    rating INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
